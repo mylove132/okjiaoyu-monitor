3bb253c05c38fa82a796df5b2939f8dc4597c3e4
刘占会 <liuzhanhui@okay.cn>
2018-11-28 11:35:49 +0800

    修改主启动文件

--- okjiaoyu-teacherspace/src/main/java/com/okjiaoyu/qa/teacherspace/main/MainTest.java
+++ okjiaoyu-teacherspace/src/main/java/com/okjiaoyu/qa/teacherspace/main/MainTest.java
@@ -8,9 +8,14 @@ import org.testng.TestNG;
  * Modify date: 2018-11-22:15:14
  */
 public class MainTest {
+    /**
+     * 项目编译后根目录
+     */
+    private static final String classPath = MainTest.class.getClassLoader().getResource("").getPath();
 
     public static void main(String[] args) {
-        String[] arg0 = new String[]{"/Users/liuzhanhui/workspace/okjiaoyu-monitor/okjiaoyu-teacherspace/target/classes/controller/login/login.xml"};
+        String loginPath = classPath+"/controller/login/login.xml";
+        String[] arg0 = new String[]{loginPath};
         TestNG.privateMain(arg0,null);
     }
 }
